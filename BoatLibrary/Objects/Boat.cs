2e3523553c2d84840e3daad1eeef1f67be22c8ec
ICOnce <noaellegaard@gmail.com>
2024-11-19 13:55:12 +0100

    Added base structure of program

---
+++ BoatLibrary/Objects/Boat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoatLibrary.Interfaces;
+
+public class Boat : IBoat
+{
+}
+
---
+++ BoatLibrary/Objects/Member.cs
@@ -0,0 +1,11 @@
+﻿using BoatLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Member : IMember
+{
+}
+
