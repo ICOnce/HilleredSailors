e9713b8cad2e3044ccca5ea1ff904d22b3f13e70
ICOnce <noaellegaard@gmail.com>
2024-12-04 11:03:26 +0100

    Added Intefaces from ClubEvent to MemberRepo from diagram

--- BoatLibrary/Interfaces/ILog.cs
+++ BoatLibrary/Interfaces/ILog.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BoatLibrary.Interfaces
+public interface ILog
 {
-    internal interface ILog
-    {
-    }
+    int EntryCount { get; }
+
+    void AddEntry(string entry);
+    string GetEntry(int id);
+    List<string> GetAll();
+    String ToString();
 }
+
