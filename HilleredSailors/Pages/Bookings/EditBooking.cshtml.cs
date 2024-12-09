b0d0c008ae0dce018c8f80a33441869bca714fb4
Katerina Rique Anastasia Østergaard <katerina@oestergaard.com>
2024-12-09 10:54:31 +0100

    Changed stuff in IBookingRepository, BookkingRepository (which is spelt wrong, thanks Magnus), and added DeleteBooking, EditBooking, ShowBooking folders, along with C# code for DeleteBooking and ShowBookings.

---
+++ HilleredSailors/Pages/Bookings/EditBooking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HilleredSailors.Pages.Bookings
+{
+    public class EditBookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
