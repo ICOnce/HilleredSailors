915a05214d88e5d1ac6112e3f6aa1b3a6f5da123
Katerina Rique Anastasia Østergaard <katerina@oestergaard.com>
2024-12-09 12:28:54 +0100

    Added folders and event-related classes.

---
+++ HilleredSailors/Pages/Events/RegisterForEvents.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HilleredSailors.Pages.Events
+{
+    public class RegisterForEventsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
