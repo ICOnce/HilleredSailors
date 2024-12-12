dc32e71b3925f91b5bab605e1fec79e459d979fa
Katerina Rique Anastasia Østergaard <katerina@oestergaard.com>
2024-12-12 14:02:06 +0100

    Contacts

---
+++ HilleredSailors/Pages/Contacts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HilleredSailors.Pages
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
