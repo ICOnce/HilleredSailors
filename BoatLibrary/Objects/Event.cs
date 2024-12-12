2ce74b9e8900cc669cbf674abb4b8fea59490a3e
Katerina Rique Anastasia Østergaard <katerina@oestergaard.com>
2024-12-12 12:08:26 +0100

    Missed #endregion

--- BoatLibrary/Objects/Event.cs
+++ BoatLibrary/Objects/Event.cs
@@ -18,8 +18,8 @@ namespace BoatLibrary.Objects
         public int ParticipantCount { get { return _participants.Count; } }
         public DateTime Date { get; set ; }
         public string header { get; set; }
-
         public string Description { get; set; }
+        #endregion
 
         #region Constructor
         public Event() { 
